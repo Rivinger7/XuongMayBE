c9ccabd478f0f83ebc9bf6cec5fd63919ac5c5ec
TDHanie <luongngocthuyduongvt@gmail.com>
2024-08-19 12:56:35 +0700

    remove unused libraries

--- XuongMay.Repositories/Mappers/OrderMapping.cs
+++ XuongMay.Repositories/Mappers/OrderMapping.cs
@@ -27,18 +27,8 @@ namespace XuongMay.Repositories.Mappers
 				.ForMember(dest => dest.EndTime, opt => opt.Ignore())
 				.ForMember(dest => dest.DeletedTime, opt => opt.Ignore());
 
-			//Ánh xạ từ AddOrderModelView sang AllOrderModelView
-			//CreateMap<AddOrderModelView, AllOrderModelView>()
-			//	.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ProductId))
-			//	.ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.ProductId))
-			//	.ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
-			//	.ForMember(dest => dest.StartTime, opt => opt.MapFrom(src => src.StartTime))
-			//	.ForMember(dest => dest.EndTime, opt => opt.MapFrom(src => src.EndTime));
-
 			//Ánh xạ từ UpdateOrderModelView sang Order
 			CreateMap<UpdateOrderModelView, Order>()
-			.ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.ProductId))
-			.ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
 			.ForMember(dest => dest.StartTime, opt => opt.Ignore())
 			.ForMember(dest => dest.EndTime, opt => opt.Ignore())
 			.ForMember(dest => dest.CreatedTime, opt => opt.Ignore())
