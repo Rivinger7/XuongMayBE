2fbad6ea96498fee90541dd22416cd749dbbf3d9
Okasu007 <Rivinger7@gmail.com>
2024-08-27 13:52:52 +0700

    feat: Code First
    InventoryHistories, InventoryChamperMappers and ChamperProducts

---
+++ XuongMay.Contract.Repositories/Entity/ChamberProducts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XuongMay.Contract.Repositories.Entity
+{
+    public class ChamberProducts
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int Capacity { get; set; }
+        public int Quantity { get; set; }
+        public required string CreatedBy { get; set; }
+        public string? LastUpdatedBy { get; set; }
+        public string? DeletedBy { get; set; }
+        public required DateTime CreatedTime { get; set; }
+        public DateTime? LastUpdatedTime { get; set; }
+        public DateTime? DeletedTime { get; set; }
+        public string? LastInventoryHistory { get; set; }
+
+        #region Entity Mapping
+        public virtual ICollection<InventoryChamberMappers>? InventoryChamberMappers { get; set; }
+        #endregion
+    }
+}
